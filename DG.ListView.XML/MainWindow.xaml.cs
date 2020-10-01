d9b7aa2851cbcd546fb0b7986cc77c46fe065313
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-10-01 13:05:05 -0500

    Commit

---
+++ DG.ListView.XML/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DG.ListView.XML
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelected(object sender, SelectionChangedEventArgs e)
+        {
+            int x = lvwEmps.Items.Count;
+            int y = lvwEmps.SelectedItems.Count;
+        }
+    }
+}
